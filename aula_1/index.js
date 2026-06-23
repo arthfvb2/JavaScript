a0d934c866f631fd0c29e43b2c1cc1e451dbd8d4
Arthur Farias <arthurfvb8@gmail.com>
2026-06-22 22:01:17 -0300

    Aula 2

--- aula_1/index.js
+++ aula_1/index.js
@@ -1,3 +1,4 @@
+//botao somar
 function somar() {
   const num1 = Number(prompt("Digite o primeiro número:"));
   const num2 = Number(prompt("Digite o segundo número:"));
@@ -5,6 +6,7 @@ function somar() {
   alert(`A soma dos números é ${soma}`);
 }
 
+//botao multiplicar
 function multiplicar() {
   const num1 = Number(prompt("Digite o primeiro número:"));
   const num2 = Number(prompt("Digite o segundo número:"));
@@ -12,10 +14,23 @@ function multiplicar() {
   alert(`A multiplicação dos números é ${multiplicacao}`);
 }
 
+//botao dividir
+function dividir() {
+  const num1 = Number(prompt("Digite o primeiro número:"));
+  const num2 = Number(prompt("Digite o segundo número:"));
+  if (num2 == 0) {
+    alert(`O segundo numero nao pode ser menor que 1`);
+  } else {
+    const divisao = num1 / num2;
+    alert(`A divisao dos números é ${divisao}`);
+  }
+}
+
+//somar inputs
 function somarInputs() {
-  const num1 = document.querySelector("#num1_soma").value;
-  const num2 = document.querySelector("#num2_soma").value;
-  console.log(num1, num2);
-  const soma = Number(num1) + Number(num2);
-  console.log(`A soma dos números é ${soma}`);
+  const num1 = Number(document.querySelector("#num1_soma").value);
+  const num2 = Number(document.querySelector("#num2_soma").value);
+  const resultado = document.querySelector("#resultado_soma");
+  const soma = num1 + num2;
+  resultado.textContent = `O resultado da soma eh ${soma}`;
 }
